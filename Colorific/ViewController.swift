ab5d50835021892d09f13d13892c1e57141e0540
Wade Sellers <Waderoni@gmail.com>
2015-11-24 02:36:14 -0700

    added didSelectRow... and a new ViewController to segue to

--- Colorific/ViewController.swift
+++ Colorific/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+
+    let colors : [UIColor] = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.purpleColor(), UIColor(red: 255/255, green: 0/255, blue: 128/255, alpha: 1.0), UIColor.blackColor()]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +23,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return colors.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
+        cell.backgroundColor = self.colors[indexPath.row]
 
         return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("toColorScreen", sender: self)
+
+        print("Table Row Selected")
     }
 
 }
