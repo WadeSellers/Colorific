604c27432d8e3bb148ce69cbdee72be8280cfd24
Wade Sellers <Waderoni@gmail.com>
2015-11-24 02:56:02 -0700

    added ColorViewController

---
+++ Colorific/ColorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ColorViewController.swift
+//  Colorific
+//
+//  Created by Wade Sellers on 11/24/15.
+//  Copyright © 2015 Apps by Wade. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
